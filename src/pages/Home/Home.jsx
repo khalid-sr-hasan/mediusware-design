5c46382d7c9300b5f3de4b75ce1ca4bf20abccd5
khalid-sr-hasan <khalid.sr.hasan@gmail.com>
2024-10-30 16:27:20 +0600

    newsletter done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,12 @@ import React, { useEffect, useState } from "react";
 import Banner from "../../components/Banner";
 import ServicesCard from "../../components/ServicesCard";
 import { IoArrowForward } from "react-icons/io5";
+import { IoIosArrowForward } from "react-icons/io";
+
+import img1 from "/images/clutch.jpg";
+import img2 from "/images/upwork.jpg";
+import img3 from "/images/google.jpg";
+import img4 from "/images/swd.jpg";
 
 const Home = () => {
     const [services, setService] = useState([]);
@@ -135,12 +141,12 @@ const Home = () => {
             {/*  */}
             <div className="bg-[#F0F8FF] py-28">
                 <div className="container mx-auto">
-                    <div className="md:w-10/12 lg:w-8/12 mx-auto ">
+                    <div className="md:w-10/12 lg:w-8/12 mx-auto mb-16">
                         <div className="text-center ">
                             <h3 className="text-xl mb-6 font-semibold  md:font-bold md:text-3xl lg:text-5xl">
-                                How We{" "}
+                                What the People {""}
                                 <span className="text-[#2EB8A2]">
-                                    Work Together
+                                    Are Saying
                                 </span>
                             </h3>
                             <p>
@@ -176,49 +182,220 @@ const Home = () => {
                                 <p className="text-sm">Software Engineer</p>
                             </div>
                         </div>
+                        <div className="w-full  border p-5 overflow-hidden bg-white rounded-lg shadow-lg">
+                            <div className="relative">
+                                <img
+                                    className="object-cover  rounded-lg w-full h-56"
+                                    src="http://surl.li/ffojfw"
+                                    alt="avatar"
+                                />
 
-                        <div className="w-full p-5 overflow-hidden bg-white rounded-lg shadow-lg">
-                            <img
-                                className="object-cover w-full h-56"
-                                src="http://surl.li/gibcek"
-                                alt="avatar"
-                            />
+                                <img
+                                    className="absolute w-12 top-1/2 left-1/2 -translate-x-1/2  -translate-y-1/2"
+                                    src="./images/play.png"
+                                    alt=""
+                                />
+                            </div>
 
-                            <div className="py-5 text-center">
-                                <a
-                                    href="#"
-                                    className="block text-xl font-bold "
-                                    tabindex="0"
-                                    role="link"
-                                >
-                                    John Doe
-                                </a>
-                                <span className="text-sm ">
-                                    Software Engineer
-                                </span>
+                            <div className="pt-5 ">
+                                <h3 className="text-lg font-medium mb-4">
+                                    Mediusware renewed our trust in outsourcing
+                                    IT development in the USA.
+                                </h3>
+                                <p>John Doe</p>
+                                <p className="text-sm">Software Engineer</p>
                             </div>
                         </div>
-                        <div className="w-full  overflow-hidden bg-white rounded-lg shadow-lg">
-                            <img
-                                className="object-cover w-full h-56"
-                                src="https://images.unsplash.com/photo-1542156822-6924d1a71ace?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-                                alt="avatar"
-                            />
+                        <div className="w-full  border p-5 overflow-hidden bg-white rounded-lg shadow-lg">
+                            <div className="relative">
+                                <img
+                                    className="object-cover  rounded-lg w-full h-56"
+                                    src="http://surl.li/ffojfw"
+                                    alt="avatar"
+                                />
 
-                            <div className="py-5 text-center">
-                                <a
-                                    href="#"
-                                    className="block text-xl font-bold text-gray-800 dark:text-white"
-                                    tabindex="0"
-                                    role="link"
-                                >
-                                    John Doe
-                                </a>
-                                <span className="text-sm text-gray-700 dark:text-gray-200">
-                                    Software Engineer
-                                </span>
+                                <img
+                                    className="absolute w-12 top-1/2 left-1/2 -translate-x-1/2  -translate-y-1/2"
+                                    src="./images/play.png"
+                                    alt=""
+                                />
+                            </div>
+
+                            <div className="pt-5 ">
+                                <h3 className="text-lg font-medium mb-4">
+                                    Mediusware renewed our trust in outsourcing
+                                    IT development in the USA.
+                                </h3>
+                                <p>John Doe</p>
+                                <p className="text-sm">Software Engineer</p>
                             </div>
                         </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* projects */}
+            <div className="container mx-auto py-28">
+                <div className="grid gap-4 grid-cols-12">
+                    <div className="col-span-12 md:col-span-10">
+                        <h3 className="text-xl mb-6 font-semibold  md:font-bold md:text-3xl lg:text-5xl">
+                            Projects We’d Hang on
+                            <span className="text-[#2EB8A2]">
+                                Our Refrigerator
+                            </span>
+                        </h3>
+                        <p>
+                            Discover our latest work, showcasing innovative
+                            solutions and successful collaborations that
+                            highlight our expertise in delivering high-quality
+                            software development projects.
+                        </p>
+                    </div>
+                    <div className="col-span-12 lg:col-span-2 lg:flex lg:justify-end lg:items-end">
+                        <button className="btn px-6 btn-info text-white">
+                            See All
+                            <IoArrowForward />
+                        </button>
+                    </div>
+                </div>
+
+                <div className="mt-16 grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+                    <div className="card card-compact  overflow-hidden  shadow-xl">
+                        <figure>
+                            <img
+                                className=" w-full  h-[250px] lg:h-[320px]"
+                                src="http://surl.li/jsfkbo"
+                                alt="Shoes"
+                            />
+                        </figure>
+                        <div className="card-body bg-[#F0F8FF]">
+                            <h2 className="card-title font-semibold">
+                                Go Thruhike Website Redesign
+                            </h2>
+                            <p>
+                                Thruhike is the ultimate platform for
+                                discovering and sharing your favorite places and
+                                products. Our mission is to celebrate local
+                                business and inspire new experiences.
+                            </p>
+                            <div className="mt-4">
+                                <button className="flex items-center gap-4 text-[#0C98EB] font-semibold">
+                                    <span> View Project</span>{" "}
+                                    <IoIosArrowForward />
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="card card-compact  overflow-hidden  shadow-xl">
+                        <figure>
+                            <img
+                                className="w-full h-[250px] lg:h-[320px]"
+                                src="http://surl.li/mcjetj"
+                                alt="Shoes"
+                            />
+                        </figure>
+                        <div className="card-body bg-[#F0F8FF]">
+                            <h2 className="card-title font-semibold">
+                                Go Thruhike Website Redesign
+                            </h2>
+                            <p>
+                                Thruhike is the ultimate platform for
+                                discovering and sharing your favorite places and
+                                products. Our mission is to celebrate local
+                                business and inspire new experiences.
+                            </p>
+                            <div className="mt-4">
+                                <button className="flex items-center gap-4 text-[#0C98EB] font-semibold">
+                                    <span> View Project</span>{" "}
+                                    <IoIosArrowForward />
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="card card-compact  overflow-hidden  shadow-xl">
+                        <figure>
+                            <img
+                                className="w-full h-[250px] lg:h-[320px]"
+                                src="http://surl.li/lipblf"
+                                alt="Shoes"
+                            />
+                        </figure>
+                        <div className="card-body bg-[#F0F8FF]">
+                            <h2 className="card-title font-semibold">
+                                Go Thruhike Website Redesign
+                            </h2>
+                            <p>
+                                Thruhike is the ultimate platform for
+                                discovering and sharing your favorite places and
+                                products. Our mission is to celebrate local
+                                business and inspire new experiences.
+                            </p>
+                            <div className="mt-4">
+                                <button className="flex items-center gap-4 text-[#0C98EB] font-semibold">
+                                    <span> View Project</span>{" "}
+                                    <IoIosArrowForward />
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/*  */}
+            <div className="bg-[#F0F8FF] py-28">
+                <div className="container mx-auto">
+                    <div className="md:w-10/12 lg:w-8/12 mx-auto ">
+                        <div className="text-center ">
+                            <h3 className="text-xl mb-6 font-semibold  md:font-bold md:text-3xl lg:text-5xl">
+                                Achievements {""}
+                                <span className="text-[#2EB8A2]">& Awards</span>
+                            </h3>
+                            <p>
+                                People know us for our top-notch work and great
+                                customer service, all of whom have expressed
+                                nothing but positive feedback about our
+                                services. Take a moment to see for yourself.
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className="mt-16 grid gap-6 grid-cols-2 md:grid-cols-4">
+                        <div className="bg-white  rounded-xl ">
+                            <img src={img1} alt="" />
+                        </div>
+                        <div className="bg-white  rounded-xl">
+                            <img src={img2} alt="" />
+                        </div>
+                        <div className="bg-white  rounded-xl">
+                            <img src={img3} alt="" />
+                        </div>
+                        <div className="bg-white  rounded-xl">
+                            <img src={img4} alt="" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* news latter */}
+            <div className="container mx-auto my-28 rounded-xl bg-[#072B4A]">
+                <div className="flex flex-col lg:flex-row justify-between items-center p-4 md:p-16">
+                    <div className="text-white">
+                        <h3 className="text-[18px] md:text-xl lg:text-3xl font-semibold">
+                            Want to know what we are up to?
+                        </h3>
+                        <p className="mt-2">Subscribe to our newsletter.</p>
+                    </div>
+
+                    <div className="flex gap-4">
+                        <input
+                            type="email"
+                            className="w-48 md:w-80 rounded-lg px-3 outline-none"
+                            placeholder="Enter your email"
+                        />
+                        <button className="btn px-6 btn-info text-white">
+                            Subscribe
+                        </button>
                     </div>
                 </div>
             </div>
